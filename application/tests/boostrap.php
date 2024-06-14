b09bf729d008fe390352c89fc48804ecb06d9150
HusseinQ7 <achmadhusseinazzaydi@gmail.com>
2024-06-14 17:38:38 +0700

    Create boostrap.php

---
+++ application/tests/boostrap.php
@@ -0,0 +1,7 @@
+<?php
+define('ENVIRONMENT', 'testing');
+define('BASEPATH', dirname(__FILE__).'/../..');
+define('APPPATH', BASEPATH.'/application/');
+define('VIEWPATH', APPPATH.'/views/');
+
+require_once BASEPATH.'/system/core/CodeIgniter.php';
